2b66c13aa370264dc17d9f75d4d2da89e96ede7a
alvaromartinezsanchez <alvaromartinezsanchez7@Gmail.com>
2020-12-22 12:01:19 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//Components
+import {SignupComponent} from './components/signup/signup.component';
+import {SigninComponent} from './components/signin/signin.component';
+import {TasksComponent} from './components/tasks/tasks.component';
+import {PrivateTasksComponent} from './components/private-tasks/private-tasks.component';
 
-const routes: Routes = [];
+import { AuthGuard } from './auth.guard';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path:'signup', component: SignupComponent },
+  { path:'signin', component: SigninComponent },
+  { path:'tasks', component: TasksComponent },
+  { path:'private', component: PrivateTasksComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
